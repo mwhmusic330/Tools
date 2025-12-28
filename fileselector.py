4b8acb852cc1dd925c76fc3087f4d004560a572f
mwhmusic330 <m.w.haburay.music@gmail.com>
2025-12-28 16:49:18 +0000

    commit from 12/28/25 lesson

--- fileselector.py
+++ fileselector.py
@@ -1,10 +1,3 @@
 import pandas as pd
 import streamlit as st
 import os
-
-
-
-def file_selectbox(dir_path='/home/michael/DATA/'):
-    filenames = os.listdir(dir_path)
-    selected_filename = st.selectbox('Select a file', filenames)
-    return os.path.join(dir_path, selected_filename)
--- intitialanalysis.py
+++ intitialanalysis.py
@@ -1,14 +1,35 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-from getdata import getdata
-from fileselector import file_selectbox
 import os
+dir_path='/home/michael/DATA/'
+filenames = os.listdir(dir_path)
+selected_filename = st.selectbox('Select a file', filenames, key = 'fileselect')
+filename = os.path.join(dir_path, selected_filename)
 
-filename = file_selectbox() 
-
+def dataloading(filename):
+    df_exists = st.session_state.get('chosendata')
+    fnmatch = st.session_state.get("filename") == filename  
+    if df_exists is not None and not df_exists.empty and fnmatch:
+        df = st.session_state.chosendata
+    elif filename.endswith(".xlsx"): 
+        excelfile = pd.ExcelFile(filename)
+        sheetname = st.selectbox('Select a sheet', excelfile.sheet_names, key = 'pageselect')
+        st.session_state.chosendata = excelfile.parse(sheet_name=sheetname) 
+        df=st.session_state.chosendata 
+    elif filename.endswith(".csv"): 
+        st.session_state.chosendata = pd.read_csv(filename)
+        df=st.session_state.chosendata
+    elif os.path.isdir(filename):
+        errormessage = "Chosen path is a directory!"
+        df = pd.DataFrame()
+        st.write(errormessage)
+        print(errormessage)
+    else:
+        raise ValueError('Invalid input please try something else!')
+    return df 
+df = dataloading(filename)
 st.title(":blue[Initial Analysis]")
-df = getdata(filename)
-###    df.info()
-###    st.dataframe(df.head())
+st.write(df.columns) ####type:ignore
+st.dataframe(df.head()) #####type:ignore
 
