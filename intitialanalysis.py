3639711cfb6dd03038cce3f822fb2a006293cad8
mwhmusic330 <m.w.haburay.music@gmail.com>
2025-12-31 14:53:59 +0000

    thought I had it but it does the same thing

--- intitialanalysis.py
+++ intitialanalysis.py
@@ -8,18 +8,19 @@ selected_filename = st.selectbox('Select a file', filenames, key = 'fileselect')
 filename = os.path.join(dir_path, selected_filename)
 
 def dataloading(filename):
-    df_exists = st.session_state.get('chosendata')
-    fnmatch = st.session_state.get("filename") == filename  
-    if df_exists is not None and not df_exists.empty and fnmatch:
-        df = st.session_state.chosendata
+####    df_exists = st.session_state.get('chosendata')
+####    fnmatch = st.session_state.get("filename") == filename  
+####    if df_exists is not None and not df_exists.empty and fnmatch:
+    if filename is not None and not st.session_state:
+        df = st.session_state[filename]
     elif filename.endswith(".xlsx"): 
         excelfile = pd.ExcelFile(filename)
         sheetname = st.selectbox('Select a sheet', excelfile.sheet_names, key = 'pageselect')
-        st.session_state.chosendata = excelfile.parse(sheet_name=sheetname) 
-        df=st.session_state.chosendata 
+        st.session_state[filename] = excelfile.parse(sheet_name=sheetname) 
+        df=st.session_state[filename]
     elif filename.endswith(".csv"): 
-        st.session_state.chosendata = pd.read_csv(filename)
-        df=st.session_state.chosendata
+        st.session_state[filename] = pd.read_csv(filename)
+        df=st.session_state[filename]
     elif os.path.isdir(filename):
         errormessage = "Chosen path is a directory!"
         df = pd.DataFrame()
